30431be9d271d80829e5bcc48e347ce30d953c4c
BenLee3 <bendesigns76@gmail.com>
2020-02-16 21:36:28 +0900

    PR update

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-import express, { static } from 'express';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const express = require('express');
 
 const app = express();
-app.use(static('build'));
+app.use(express.static('build'));
 
 // set
 app.set('port', 9090);
